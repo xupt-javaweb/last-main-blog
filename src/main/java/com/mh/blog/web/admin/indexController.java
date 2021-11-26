2754f7e74b42fe519e27aad6a6e3f60ecca9fb0a
zhangjiaqi-coldplay <714479723@qq.com>
2021-11-26 18:17:15 +0800

    张嘉祺上传

---
+++ src/main/java/com/mh/blog/web/admin/indexController.java
@@ -0,0 +1,11 @@
+//package com.mh.blog.web.admin;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/admin")
+//public class indexController {
+//
+//
+//}
